ab170b1999e9e4469fa1ba5e777425441236ccc4
bing <bindaguo@gmail.com>
2018-03-30 01:23:14 +0800

    update quiz

--- src/quiz.java
+++ src/quiz.java
@@ -3,10 +3,16 @@ public class quiz {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 	    int input = sc.nextInt();
-	    int sum = 0;
-	    while(input != 0) {
-			sum += input%10;
-			input /= 10;
-		}
+	    System.out.print(input+" ");
+	    
+	    while(input > 10){
+	    	int sum = 0;
+		    while(input != 0) {
+				sum += input%10;
+				input /= 10;
+			}
+		    input = sum;
+		    System.out.print(input+" ");
+	    }
 	}
 }
